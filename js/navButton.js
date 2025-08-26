804061bd8c0e7984c268937e41fd10041e238175
Guilherme Silveira Emanuelli <gui.emanuelli@hotmail.com>
2025-08-25 22:24:09 -0300

    Revisão e Comentários
    Revisão de todos o HTML, CSS e Javascript
    Adição de comentários em arquivos CSS e Javascript

--- js/navButton.js
+++ js/navButton.js
@@ -1,7 +1,9 @@
 
+// Pega do HTML o elemento do botão e da barra de navegação
 var navButton = document.getElementById('nav_button-container');
 var navBar = document.getElementById("nav-container");
 
+// Adiciona no botão o evento de aparecer e desaparecer a barra de navegação
 navButton.addEventListener("click", () => {
   if (navBar.style.display === "block") {
     navBar.style.display = "none";
